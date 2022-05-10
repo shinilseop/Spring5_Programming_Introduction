99351eeeba609f8fb540dd37aed4c4aad8865968
shinilseop <shinilseop12@gmail.com>
2022-05-11 02:23:13 +0900

    Initial commit

---
+++ src/main/java/chap03/DuplicateMemberException.java
@@ -0,0 +1,7 @@
+package chap03;
+
+public class DuplicateMemberException extends RuntimeException {
+    public DuplicateMemberException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/chap03/Main.java
@@ -0,0 +1,10 @@
+package chap03;
+
+public class Main {
+    public static void main(String[] args) {
+        MemberDao memberDao = new MemberDao();
+        MemberRegisterService regSvc = new MemberRegisterService(memberDao);
+        ChangePasswordService pwdSvc = new ChangePasswordService();
+        pwdSvc.setMemberDao(memberDao);
+    }
+}
